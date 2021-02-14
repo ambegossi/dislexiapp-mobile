b64cd17de27ea0401d0c169b02a8a1f37ee26e6e
Anderson Begossi <andersonbegossi@gmail.com>
2021-02-14 20:15:57 -0300

    :building_construction:(chore) configure navigation

--- src/App.js
+++ src/App.js
@@ -2,17 +2,22 @@ import 'react-native-gesture-handler';
 
 import React from 'react';
 import { View, StatusBar } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+
+import Routes from './routes';
 
 const App = () => (
-  <>
+  <NavigationContainer>
     <StatusBar barStyle="light-content" backgroundColor="#212765" />
     <View
       style={{
         flex: 1,
         backgroundColor: '#212765',
       }}
-    />
-  </>
+    >
+      <Routes />
+    </View>
+  </NavigationContainer>
 );
 
 export default App;
