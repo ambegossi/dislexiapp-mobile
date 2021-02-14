f80b4c1c8b7f08069dc07879bcb2cc86b814258b
Anderson Begossi <andersonbegossi@gmail.com>
2021-02-14 20:13:26 -0300

    :sparkles:(feat) create auth pages

---
+++ src/pages/SignIn/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+const SignIn = () => {
+  return <Container />;
+};
+
+export default SignIn;
---
+++ src/pages/SignUp/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+const SignUp = () => {
+  return <Container />;
+};
+
+export default SignUp;
