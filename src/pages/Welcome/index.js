b9f038829679aec4623400d9e757090314de7adf
Anderson Begossi <andersonbegossi@gmail.com>
2021-02-20 15:20:21 -0300

    :sparkles:(feat) create Welcome page

--- src/pages/Welcome/index.js
+++ src/pages/Welcome/index.js
@@ -1,9 +1,35 @@
 import React from 'react';
+import LottieView from 'lottie-react-native';
+import { useNavigation } from '@react-navigation/native';
 
-import { Container } from './styles';
+import logoImg from '../../assets/images/logo.png';
+import astronaut from '../../assets/animations/astronaut.json';
+
+import {
+  Container,
+  Image,
+  Button,
+  SignInButton,
+  SignInButtonText,
+} from './styles';
 
 const Welcome = () => {
-  return <Container />;
+  const navigation = useNavigation();
+
+  return (
+    <>
+      <Container colors={['rgba(33,39,101,1)', 'rgba(49,55,118,1)']}>
+        <LottieView source={astronaut} autoPlay loop style={{ width: 250 }} />
+
+        <Image source={logoImg} />
+
+        <Button onPress={() => navigation.navigate('SignUp')}>Cadastrar</Button>
+      </Container>
+      <SignInButton onPress={() => navigation.navigate('SignIn')}>
+        <SignInButtonText>Entrar</SignInButtonText>
+      </SignInButton>
+    </>
+  );
 };
 
 export default Welcome;
--- src/pages/Welcome/styles.js
+++ src/pages/Welcome/styles.js
@@ -1,3 +1,41 @@
 import styled from 'styled-components/native';
+import LinearGradient from 'react-native-linear-gradient';
+import { getBottomSpace } from 'react-native-iphone-x-helper';
 
-export const Container = styled.View``;
+import ButtonComponent from '../../components/Button';
+
+export const Container = styled(LinearGradient)`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+  padding: 0 30px;
+`;
+
+export const Image = styled.Image`
+  margin-top: 20px;
+  width: 160px;
+`;
+
+export const Button = styled(ButtonComponent)`
+  margin-top: 60px;
+  margin-bottom: 40px;
+`;
+
+export const SignInButton = styled.TouchableOpacity`
+  position: absolute;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  border-top-width: 1px;
+  border-color: #adadad;
+  padding: 16px 0 ${16 + getBottomSpace()}px;
+
+  justify-content: center;
+  align-items: center;
+`;
+
+export const SignInButtonText = styled.Text`
+  color: #fff;
+  font-size: 18px;
+  font-family: 'Nunito-SemiBold';
+`;
