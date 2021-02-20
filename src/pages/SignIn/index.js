ed07eb8bbf7effd363bb380be47d71178ab93fa6
Anderson Begossi <andersonbegossi@gmail.com>
2021-02-20 16:00:38 -0300

    :sparkles:(feat) create SignIn page

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,9 +1,74 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import {
+  ScrollView,
+  KeyboardAvoidingView,
+  Platform,
+  Keyboard,
+} from 'react-native';
+import Icon from 'react-native-vector-icons/Feather';
+import { useNavigation } from '@react-navigation/native';
 
-import { Container } from './styles';
+import Input from '../../components/Input';
+
+import logoImg from '../../assets/images/logo.png';
+
+import { Container, Button, Image, BackButton, BackButtonText } from './styles';
 
 const SignIn = () => {
-  return <Container />;
+  const navigation = useNavigation();
+
+  const [isKeyboardVisible, setKeyboardVisible] = useState(false);
+
+  useEffect(() => {
+    const keyboardDidShowListener = Keyboard.addListener(
+      'keyboardDidShow',
+      () => {
+        setKeyboardVisible(true);
+      },
+    );
+    const keyboardDidHideListener = Keyboard.addListener(
+      'keyboardDidHide',
+      () => {
+        setKeyboardVisible(false);
+      },
+    );
+
+    return () => {
+      keyboardDidHideListener.remove();
+      keyboardDidShowListener.remove();
+    };
+  }, []);
+
+  return (
+    <>
+      <KeyboardAvoidingView
+        style={{ flex: 1 }}
+        behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+        enabled
+      >
+        <ScrollView
+          keyboardShouldPersistTaps="handled"
+          contentContainerStyle={{ flex: 1 }}
+        >
+          <Container colors={['rgba(33,39,101,1)', 'rgba(49,55,118,1)']}>
+            <Image source={logoImg} />
+
+            <Input name="name" icon="user" placeholder="Nome" />
+
+            <Input name="password" icon="lock" placeholder="Senha" />
+
+            <Button onPress={() => { }}>Entrar</Button>
+          </Container>
+        </ScrollView>
+      </KeyboardAvoidingView>
+      {!isKeyboardVisible && (
+        <BackButton onPress={() => navigation.goBack()}>
+          <Icon name="arrow-left" size={20} color="#fff" />
+          <BackButtonText>Voltar</BackButtonText>
+        </BackButton>
+      )}
+    </>
+  );
 };
 
 export default SignIn;
