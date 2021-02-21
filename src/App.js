feb5c6292bb842118c0d88aef1605c84998e3968
Anderson Begossi <andersonbegossi@gmail.com>
2021-02-21 19:24:24 -0300

    :recycle:(chore) add AppProvider in the App.js

--- src/App.js
+++ src/App.js
@@ -4,19 +4,23 @@ import React from 'react';
 import { View, StatusBar } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 
+import AppProvider from './hooks';
+
 import Routes from './routes';
 
 const App = () => (
   <NavigationContainer>
     <StatusBar barStyle="light-content" backgroundColor="#212765" />
-    <View
-      style={{
-        flex: 1,
-        backgroundColor: '#212765',
-      }}
-    >
-      <Routes />
-    </View>
+    <AppProvider>
+      <View
+        style={{
+          flex: 1,
+          backgroundColor: '#212765',
+        }}
+      >
+        <Routes />
+      </View>
+    </AppProvider>
   </NavigationContainer>
 );
 
