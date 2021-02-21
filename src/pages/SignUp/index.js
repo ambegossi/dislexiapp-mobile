709fc3183fb4f07d662e9f19e8f0311dad8f516f
Anderson Begossi <andersonbegossi@gmail.com>
2021-02-21 13:21:37 -0300

    :sparkles:(feat) integrate SignUp page with react hook form and yup

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import {
   ScrollView,
   KeyboardAvoidingView,
@@ -7,6 +7,9 @@ import {
 } from 'react-native';
 import Icon from 'react-native-vector-icons/Feather';
 import { useNavigation } from '@react-navigation/native';
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from 'yup';
 
 import Input from '../../components/Input';
 
@@ -14,10 +17,32 @@ import logoImg from '../../assets/images/logo.png';
 
 import { Container, Button, Image, BackButton, BackButtonText } from './styles';
 
+const schema = yup.object().shape({
+  name: yup.string().required('O nome é obrigatório'),
+  password: yup
+    .string()
+    .required('A senha é obrigatória')
+    .min(6, 'A senha deve ter no mínimo 6 dígitos'),
+  passwordConfirmation: yup
+    .string()
+    .required('A confirmação é obrigatória')
+    .oneOf([yup.ref('password'), null], 'As senhas devem corresponder'),
+});
+
 const SignUp = () => {
   const navigation = useNavigation();
 
+  const { control, handleSubmit, errors } = useForm({
+    resolver: yupResolver(schema),
+  });
+
+  const nameInputRef = useRef();
+  const passwordInputRef = useRef();
+  const passwordConfirmationInputRef = useRef();
+
   const [isKeyboardVisible, setKeyboardVisible] = useState(false);
+
+  const onSubmit = data => console.log(data);
 
   useEffect(() => {
     const keyboardDidShowListener = Keyboard.addListener(
@@ -53,17 +78,37 @@ const SignUp = () => {
           <Container colors={['rgba(33,39,101,1)', 'rgba(49,55,118,1)']}>
             <Image source={logoImg} />
 
-            <Input name="name" icon="user" placeholder="Nome" />
-
-            <Input name="password" icon="lock" placeholder="Senha" />
-
             <Input
-              name="password-confirmation"
-              icon="lock"
-              placeholder="Confirme a senha"
+              autoCorrect={false}
+              name="name"
+              icon="user"
+              placeholder="Nome"
+              control={control}
+              errors={errors}
+              ref={nameInputRef}
             />
 
-            <Button onPress={() => { }}>Cadastrar</Button>
+            <Input
+              name="password"
+              icon="lock"
+              placeholder="Senha"
+              control={control}
+              errors={errors}
+              ref={passwordInputRef}
+              secureTextEntry
+            />
+
+            <Input
+              name="passwordConfirmation"
+              icon="lock"
+              placeholder="Confirme a senha"
+              control={control}
+              errors={errors}
+              ref={passwordConfirmationInputRef}
+              secureTextEntry
+            />
+
+            <Button onPress={handleSubmit(onSubmit)}>Cadastrar</Button>
           </Container>
         </ScrollView>
       </KeyboardAvoidingView>
