c8206feb02327bc85cade726be5df8951278d223
Anderson Begossi <andersonbegossi@gmail.com>
2021-03-03 09:29:46 -0300

    :sparkles:(feat) create Profile page

---
+++ src/pages/Profile/index.js
@@ -0,0 +1,163 @@
+import React, { useRef, useState } from 'react';
+import {
+  KeyboardAvoidingView,
+  ScrollView,
+  Platform,
+  TouchableOpacity,
+  Alert,
+} from 'react-native';
+import Icon from 'react-native-vector-icons/Feather';
+import { useNavigation } from '@react-navigation/native';
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from 'yup';
+
+import Input from '../../components/Input';
+
+import bgImg from '../../assets/images/bg.png';
+import { useAuth } from '../../hooks/auth';
+import capitalizeFirstLetter from '../../utils/capitalizeFirstLetter';
+import api from '../../services/api';
+
+import {
+  Container,
+  Header,
+  AvatarWrapper,
+  UserAvatar,
+  Button,
+  ProgressBar,
+  UserProgress,
+} from './styles';
+
+const schema = yup.object().shape({
+  name: yup.string().required('O nome é obrigatório'),
+  password: yup
+    .string()
+    .required('A senha é obrigatória')
+    .min(6, 'A senha deve ter no mínimo 6 dígitos'),
+  password_confirmation: yup
+    .string()
+    .required('A confirmação é obrigatória')
+    .oneOf([yup.ref('password'), null], 'As senhas devem corresponder'),
+});
+
+const Profile = () => {
+  const navigation = useNavigation();
+
+  const { user, updateUser } = useAuth();
+
+  const [loading, setLoading] = useState(false);
+
+  const { control, handleSubmit, errors } = useForm({
+    resolver: yupResolver(schema),
+    defaultValues: {
+      name: capitalizeFirstLetter(user.name),
+    },
+  });
+
+  const nameInputRef = useRef();
+  const passwordInputRef = useRef();
+  const passwordConfirmationInputRef = useRef();
+
+  const onSubmit = async data => {
+    setLoading(true);
+    try {
+      const response = await api.put('/profile', data);
+
+      updateUser(response.data);
+
+      Alert.alert('Perfil atualizado com sucesso!');
+
+      navigation.navigate('Dashboard');
+    } catch (err) {
+      Alert.alert(
+        'Erro ao salvar o perfil',
+        err.response?.data?.message ||
+          'Ocorreu um erro ao atualizar o perfil, tente novamente.',
+      );
+    }
+    setLoading(false);
+  };
+
+  return (
+    <KeyboardAvoidingView
+      style={{ flex: 1 }}
+      behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+      enabled
+    >
+      <ScrollView keyboardShouldPersistTaps="handled" style={{ flex: 1 }}>
+        <Container source={bgImg}>
+          <Header>
+            <TouchableOpacity onPress={() => navigation.goBack()}>
+              <Icon name="arrow-left-circle" size={32} color="#fff" />
+            </TouchableOpacity>
+          </Header>
+
+          <AvatarWrapper>
+            <TouchableOpacity onPress={() => {}}>
+              <UserAvatar
+                source={{
+                  uri:
+                    'https://ramcotubular.com/wp-content/uploads/default-avatar.jpg',
+                }}
+              />
+            </TouchableOpacity>
+
+            <ProgressBar>
+              <UserProgress />
+            </ProgressBar>
+          </AvatarWrapper>
+
+          <Input
+            autoCorrect={false}
+            autoCapitalize="words"
+            name="name"
+            icon="user"
+            placeholder="Nome"
+            control={control}
+            errors={errors}
+            ref={nameInputRef}
+            returnKeyType="next"
+            onSubmitEditing={() => {
+              passwordInputRef.current?.focus();
+            }}
+          />
+
+          <Input
+            name="password"
+            icon="lock"
+            placeholder="Senha"
+            control={control}
+            errors={errors}
+            ref={passwordInputRef}
+            secureTextEntry
+            returnKeyType="next"
+            onSubmitEditing={() => {
+              passwordConfirmationInputRef.current?.focus();
+            }}
+            textContentType="newPassword"
+          />
+
+          <Input
+            name="password_confirmation"
+            icon="lock"
+            placeholder="Confirme a senha"
+            control={control}
+            errors={errors}
+            ref={passwordConfirmationInputRef}
+            secureTextEntry
+            returnKeyType="send"
+            onSubmitEditing={handleSubmit(onSubmit)}
+            textContentType="newPassword"
+          />
+
+          <Button loading={loading} onPress={handleSubmit(onSubmit)}>
+            Salvar
+          </Button>
+        </Container>
+      </ScrollView>
+    </KeyboardAvoidingView>
+  );
+};
+
+export default Profile;
---
+++ src/pages/Profile/styles.js
@@ -0,0 +1,50 @@
+import styled from 'styled-components/native';
+import { getStatusBarHeight } from 'react-native-iphone-x-helper';
+
+import ButtonComponent from '../../components/Button';
+
+export const Container = styled.ImageBackground`
+  flex: 1;
+  padding: 0 30px;
+`;
+
+export const Header = styled.View`
+  padding: ${getStatusBarHeight()}px 0 30px 0;
+
+  align-self: flex-start;
+`;
+
+export const AvatarWrapper = styled.View`
+  margin-bottom: 30px;
+
+  flex-direction: row;
+  align-items: center;
+  align-self: flex-start;
+`;
+
+export const UserAvatar = styled.Image`
+  width: 80px;
+  height: 80px;
+  border-radius: 40px;
+  border-width: 3px;
+  border-color: #be1fd3;
+  margin-right: 20px;
+`;
+
+export const Button = styled(ButtonComponent)`
+  margin-top: 15px;
+`;
+
+export const ProgressBar = styled.View`
+  width: 125px;
+  height: 20px;
+  background-color: #fff;
+  border-radius: 10px;
+`;
+
+export const UserProgress = styled.View`
+  width: 40px;
+  height: 20px;
+  border-radius: 10px;
+  background-color: #04d361;
+`;
