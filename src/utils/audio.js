7d63f1106f1e1b59da3d021bbe15bd5b3ef3025a
Anderson Begossi <andersonbegossi@gmail.com>
2021-04-24 16:47:01 -0300

    :adhesive_bandage:(chore) check if the audio should be loaded from the app bundle

--- src/utils/audio.js
+++ src/utils/audio.js
@@ -1,13 +1,17 @@
 import Sound from 'react-native-sound';
 
-export const playAudio = audioPath => {
-  const sound = new Sound(audioPath, Sound.MAIN_BUNDLE, error => {
-    if (error) {
+export const playAudio = (audioPath, isMainBundle) => {
+  const sound = new Sound(
+    audioPath,
+    isMainBundle ? Sound.MAIN_BUNDLE : '',
+    error => {
+      if (error) {
+        return null;
+      }
+
+      sound.play();
+
       return null;
-    }
-
-    sound.play();
-
-    return null;
-  });
+    },
+  );
 };
